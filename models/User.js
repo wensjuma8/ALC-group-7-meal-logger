5c36f626d5a089cba35606e3cfe6d339f9b2a5f4
Moses Okocha <okocha.chukwutem@gmail.com>
2018-05-30 01:06:58 +0100

    POST /register and POST/login functionality added

--- models/User.js
+++ models/User.js
@@ -4,8 +4,18 @@ const Schema = mongoose.Schema;
 //Create User Schema
 const UserSchema = new Schema({
   //Define properties here
+  name: {
+    required: true,
+    type: String
+  },
+  email: {
+    required: true,
+    type: String
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = {
-  User: new mongoose.model("User", UserSchema)
-};
+module.exports = User = mongoose.model("User", UserSchema);
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+
+const User = require("../models/User");
 
 // @route   GET '/register'
 // desc     Get register route
@@ -9,12 +12,64 @@ router.get("/register", (req, res) => res.render("register.hbs"));
 // @route   POST '/register'
 // desc     Post to register route
 // access   Public
-router.post("/register");
+router.post("/register", (req, res) => {
+  const body = req.body;
+
+  User.findOne({ email: body.email }).then(user => {
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "This email is already registered" });
+    } else {
+      const newUser = new User({
+        name: body.name,
+        email: body.email,
+        password: body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
 
 // @route   GET '/'
 // desc     Get register route | This acts as homepage and login page
 // access   Public
 router.get("/", (req, res) => res.render("home.hbs"));
+
+// @route   POST '/'
+// desc     Get register route | This acts as homepage and login page
+// access   Public
+router.post("/", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      //Check if user exists
+      if (!user) {
+        return res.sendStatus(404).json({ error: "User not found" });
+      }
+      //Check if credentials match
+      bcrypt
+        .compare(password, user.password)
+        .then(authenticated => {
+          if (authenticated) {
+            return res.json({ success: "User has been authenticated" });
+          }
+          return res.json({ error: "Invalid email/password combination" });
+        })
+        .catch(err => console.log(err));
+    })
+    .catch(err => console.log(err));
+});
 
 // @route   GET '/meals'
 // desc     Get all meals route
